c6fe1ccbb533af306349e698087ac27618a0661b
Oliver Valls <tramuntanal@gmail.com>
2016-10-17 14:47:34 +0200

    Update version to 0.1.0. (#5)

--- lib/ble/version.rb
+++ lib/ble/version.rb
@@ -1,9 +1,15 @@
 module BLE
-  # Library version
+  # = Library version =
+  # Major version zero (0.y.z) is for initial development.
+  # Anything may change at any time.
+  # The public API should not be considered stable.
 
-  #
+  # 0.1.0 MINOR
+  # - [FEATURE] BLE notifications functionality.
+  # - [REFACTOR] Breaks Characteristic API
+
   # 0.0.3 PATCH
   # - [FIX] NotSupported exeption was not declared.
   #
-  VERSION = '0.0.3'
+  VERSION = '0.1.0'
 end
